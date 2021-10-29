3272e3457586d9c5901973cb5db2b277854e67bd
mfan <marcuscmfan2@gmail.com>
2021-10-28 18:14:49 -0700

    Scanner Tutorial Finished

--- src/ciseleven/scannertut/ScannerTutorial.java
+++ src/ciseleven/scannertut/ScannerTutorial.java
@@ -10,8 +10,31 @@ public class ScannerTutorial {
          */
         Scanner s = new Scanner(System.in);
 
-        //there are many different scanners
+        /**
+         * Scanners scan for many things the following are the different basic Scanner methods and
+         * some examples.
+         */
+
+        //Scans for a the line returns a String
         s.nextLine();
-        //Scans
+
+        String stringNextLine = s.nextLine();
+        System.out.println("You used s.nextLine() and it read " + stringNextLine);
+        //Scans for Integer value
+        s.nextInt();
+
+        int intNextInt = s.nextInt();
+        System.out.println("You used s.nextInt() and it read " + intNextInt + ", you can use anything to manipulate the variable like intNextLine + 1 = " +(intNextInt + 1));
+        //Scans for Double value
+        s.nextDouble();
+
+        double doubleNextDouble = s.nextDouble();
+        System.out.println("You used s.nextDouble() to find " + doubleNextDouble);
+        //Scans for String until next break
+        s.next();
+
+        String stringNext = s.next();
+        System.out.println("You used s.next to read your first word : " + stringNext + ", but if you wrote a sentence you would still only see the first word");
+
     }
 }
